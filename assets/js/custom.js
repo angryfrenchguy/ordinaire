1e0cad26ee404a9ce23e26266696b9537fad82d8
angryfrenchguy <angryfrenchguy@gmail.com>
2018-06-03 20:15:56 -0400

    Coloriage automatique du tableau de stationnement

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,18 @@
 // Custom scripts
+
+function rougeVert() {
+  var span = document.getElementsByTagName('span');
+
+  for ( var i=0 ; i < span.length; i++ ){
+     // span[i].style.color = "pink";
+     // span[i].style.padding = '0.5em';
+     if (/\bNon\b/i.test(span[i].innerHTML)) {
+         span[i].style.color= 'red';
+       } else if (/\bOui\b/i.test(span[i].innerHTML)) {
+         span[i].style.color= 'green';
+       }
+       // else {
+       //   span[i].style.color= 'yellow';
+       // }
+  }
+}
