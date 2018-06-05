e2ed978c4c4f2ad48794f7fb9a702f5b8cbb9ef0
angryfrenchguy <angryfrenchguy@gmail.com>
2018-06-05 10:56:31 -0400

    Full text search (pis aparament un update de tout le site)

--- assets/js/main.js
+++ assets/js/main.js
@@ -8784,3 +8784,16 @@
         }
     })(window);
 })();
+
+function rougeVert() {
+    var span = document.getElementsByTagName("span");
+    for (var i = 0; i < span.length; i++) {
+        if (/\bNon\b/i.test(span[i].innerHTML)) {
+            span[i].style.color = "red";
+        } else if (/\bOui\b/i.test(span[i].innerHTML)) {
+            span[i].style.color = "green";
+        } else if (/\u00C0\s\bvos\b\s\brisques\b/i.test(span[i].innerHTML)) {
+            span[i].style.color = "orange";
+        }
+    }
+}
