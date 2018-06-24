21edd1210c11b1e7482143b5a991db35e89af401
angryfrenchguy <angryfrenchguy@gmail.com>
2018-06-23 21:06:08 -0400

    Pas pires améliorations à la mise en page des formulaires

--- assets/js/signature.js
+++ assets/js/signature.js
@@ -9,8 +9,10 @@ function signature() {
   var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
     penColor: 'rgb(40, 60, 131)'
   });
+
   var saveButton = document.getElementById('save');
   var cancelButton = document.getElementById('clear');
+  var backdeux = document.getElementById('backdeux');
 
   saveButton.addEventListener('click', function(event) {
     var sig = signaturePad.toDataURL('image/png');
@@ -21,7 +23,33 @@ function signature() {
     signaturePad.clear();
   });
 
-  // console.log('allo');
+  backdeux.addEventListener('click', function(event) {
+    boutons.style.display = 'none';
+    texte.style.display = 'inline';
+  });
+
+  // Option UN de resize Canvas
+
+  // function resizeCanvas() {
+  //   console.log(this); // I should always be the canvas element
+  //   var ratio = Math.max(window.devicePixelRatio || 1, 1);
+  //   this.width = this.offsetWidth * ratio;
+  //   this.height = this.offsetHeight * ratio;
+  //   this.getContext("2d").scale(ratio, ratio);
+  // }
+  // window.onresize = resizeCanvas.bind(document.querySelector('canvas'));
+
+    // Option DEUX de resize Canvas
+
+  //   function resizeCanvas() {
+  //     var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+  //     canvas.width = canvas.offsetWidth * ratio;
+  //     canvas.height = canvas.offsetHeight * ratio;
+  //     canvas.getContext("2d").scale(ratio, ratio);
+  //     signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+  // }
+  //
+  // window.addEventListener("resize", resizeCanvas);
 
 };
 
@@ -29,12 +57,12 @@ function image(sig) {
 
   var logo = new Image();
   logo.onload = function() {
-      fairePDF(sig,logo);
+    fairePDF(sig, logo);
   };
   logo.src = '/../../assets/img/excellence.png';
 }
 
-function fairePDF(sig,logo) {
+function fairePDF(sig, logo) {
 
   var titre = document.getElementById('titre').innerHTML;
   var texte = document.getElementById('textecite').innerHTML;
@@ -43,9 +71,10 @@ function fairePDF(sig,logo) {
   var courriel = document.getElementsByName('courriel')[0].value;
   var date = moment().format('D MMMM YYYY');
 
-    toPDF(sig, titre, texte, nom, courriel, date, contrat, logo);
+  toPDF(sig, titre, texte, nom, courriel, date, contrat, logo);
+  console.log(sig, titre, texte, nom, courriel, date, contrat, logo)
 
-    // console.log(logo);
+  // console.log(logo);
 
 }
 
@@ -87,12 +116,49 @@ function toPDF(sig, titre, texte, nom, courriel, date, contrat, logo) {
       dataType: 'json'
     }).done(function(reponse) {
       console.log(reponse);
-      window.open("../../merci.html","_self")
+      window.open("../../merci.html", "_self")
     });
   }
 
 }
 
+function next() {
+  // var titre = document.getElementById('titre').innerHTML;
+  // var texte = document.getElementById('textecite').innerHTML;
+  var nextButton = document.getElementById('next');
+
+  nextButton.addEventListener('click', function(event) {
+    texte.style.display = 'inline';
+    infodiv.style.display = 'none';
+    back.style.display = "block";
+    // var contrat = 're. contrat ' + document.getElementsByName('contrat')[0].value;
+    // var nom = document.getElementsByName('nom')[0].value;
+    // var courriel = document.getElementsByName('courriel')[0].value;
+    // var date = moment().format('D MMMM YYYY');
+    // suite(contrat,nom,courriel);
+    suite();
+  });
+}
+
+function suite() {
+
+  var backButton = document.getElementById('back');
+  var okButton = document.getElementById('ok');
+
+  okButton.addEventListener('click', function(event) {
+    texte.style.display = 'none';
+    boutons.style.display = 'block';
+  });
+
+  backButton.addEventListener('click', function(event) {
+    texte.style.display = 'none';
+    infodiv.style.display = 'inline';
+  });
+}
+
+
 $(document).ready(function() {
+  // signature();
+  next();
   signature();
 });
