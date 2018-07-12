7cfc39ae3c4eb4777f229458877cca30d0286bd2
angryfrenchguy <angryfrenchguy@gmail.com>
2018-07-12 11:12:13 -0400

    Celle où je mets Martin Godbout en CC pour le live.

--- php/mail-bus.php
+++ php/mail-bus.php
@@ -1,4 +1,4 @@
-  <?php
+<?php
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -19,45 +19,52 @@ $titre = $json['titre'];
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'mail.net-c.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'general@netc.fr';                 // SMTP username
-    $mail->Password = 'bTaxbfkj3cdBt9ApABNZ';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+  //Server settings
+  $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  //$mail->Host = 'mail.net-c.com';  // Specify main and backup SMTP servers
+$mail->Host = 'mail.gandi.net';
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  //$mail->Username = 'general@netc.fr';
+$mail->Username = 'autobus@autobus.xyz';                 // SMTP username
+    //$mail->Password = 'bTaxbfkj3cdBt9ApABNZ';
+  $mail->Password = '7MJpmHm/qWzfmGNr';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 587;                                    // TCP port to connect to
 
-    //Recipients
-    $mail->setFrom('general@netc.fr', 'Autobus');
-    $mail->addAddress($courriel);     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('general@netc.fr', 'email du boss');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+  //Recipients
+  //$mail->setFrom('general@netc.fr', 'Autobus');
+  $mail->setFrom('autobus@autobus.xyz', 'Autobus');
+  $mail->addAddress($courriel);     // Add a recipient
+  // Tu peux commenter out ces deux adresse pour faire des tests
+  $mail->addAddress('marting@autocarexcellence.com');     // Add a recipient
+  $mail->addReplyTo('marting@autocarexcellence.com');
+  //$mail->addAddress('ellen@example.com');               // Name is optional
+  //$mail->addReplyTo('general@netc.fr', 'email du boss');
+  //$mail->addCC('zing@dispostable.com');
+  //$mail->addBCC('bcc@example.com');
 
-    //Attachments
-    $base = explode('data:application/pdf;base64,', $_POST['data']);
-    $base = base64_decode($base[1]);
-    $mail->addStringAttachment($base, 'autorisation.pdf');
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  //Attachments
+  $base = explode('data:application/pdf;base64,', $_POST['data']);
+  $base = base64_decode($base[1]);
+  $mail->addStringAttachment($base, 'autorisation.pdf');
+  // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+  // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = $titre . ' ' . $contrat;
-    $mail->Body    = $titre;
-    $mail->AltBody = $titre;
+  //Content
+  $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = $titre . ' ' . $contrat;
+  $mail->Body    = $titre;
+  $mail->AltBody = $titre;
 
-    $mail->send();
-    echo 'Message has been sent';
+  $mail->send();
+  echo 'Message has been sent';
 } catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-  };
+  echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+};
 
-  // TODO Voir à ceci, pris sur le README
-  // To load the French version
-  // $mail->setLanguage('fr', '/optional/path/to/language/directory/');
+// TODO Voir à ceci, pris sur le README
+// To load the French version
+// $mail->setLanguage('fr', '/optional/path/to/language/directory/');
 
-  // TODO Est-ce que je peux configurer ceci autrement, de façon à ne pas avoir tout le dossier Vendor dans le site compilé?
+// TODO Est-ce que je peux configurer ceci autrement, de façon à ne pas avoir tout le dossier Vendor dans le site compilé?
